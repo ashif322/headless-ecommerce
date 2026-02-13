977084351a1b7c9b547746a4c5ab1ed359742913
ashif laravel <ashif.322@webkul.in>
2026-02-13 12:00:51 +0530

    Add Invoice download url key

--- src/Queries/Shop/Customer/InvoiceQuery.php
+++ src/Queries/Shop/Customer/InvoiceQuery.php
@@ -4,9 +4,18 @@ namespace Webkul\GraphQLAPI\Queries\Shop\Customer;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\URL;
+use Webkul\Sales\Repositories\InvoiceRepository;
 
 class InvoiceQuery
 {
+    /**
+     * Create a new instance.
+     */
+    public function __construct(
+        protected InvoiceRepository $invoiceRepository
+    ) {}
+
     /**
      * Filter query for order invoices.
      */
@@ -37,5 +46,27 @@ class InvoiceQuery
         return $query->whereHas('order', function ($query) use ($customer) {
             $query->where('customer_id', $customer->id);
         });
+    }
+
+    /**
+     * Generate URL for invoice PDF download.
+     */
+    public function getInvoiceUrl($invoice): ?string
+    {
+        try {
+            $invoiceModel = $this->invoiceRepository->find($invoice->id);
+
+            if (! $invoiceModel) {
+                return null;
+            }
+
+            return URL::signedRoute(
+                'shop.customers.account.orders.print-invoice',
+                ['id' => $invoice->id]
+            );
+
+        } catch (\Exception $e) {
+            return null;
+        }
     }
 }
